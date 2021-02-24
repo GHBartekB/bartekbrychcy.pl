494e2289f0dda82a12f925157f2586772a778496
Bartek Brychcy <bbrychcy@onet.eu>
2021-02-24 12:47:36 +0100

    change file structure install and config MiniCssExtractPlugin to dev mode

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 
@@ -17,11 +18,12 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use:[ 'style-loader', 'css-loader'],
-
-                test: /\.(scss|sass)$/,
-                use: ['style-loader', 'sass-loader']
+                use:[MiniCssExtractPlugin.loader, 'css-loader'],
             },  
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
+            }
 
         ]
 
@@ -33,6 +35,9 @@ module.exports = {
 
         }),
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name]-[contenthash:6].css',
+        })
     ]
 
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log('test');
+
+import './sass/style.scss';
